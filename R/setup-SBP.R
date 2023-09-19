9158fe99f6b3bb18cabcb4d4b85ede25af0b0cbc
hmashrique <hmshrque@memphis.edu>
2023-09-19 13:20:28 -0500

    initial files commit

---
+++ R/setup-SBP.R
@@ -0,0 +1,27 @@
+
+SBP.code.path="https://raw.githubusercontent.com/stan-pounds/Simple-Biostats-Program/main/"
+
+# specify the file names
+SBP.code.files=c("get-packages.R",
+               "describe.R","estimate.R","correlate.R",
+               "compare.R","model.R","colors.R",
+               "print.R","figures.R","read-data.R",
+               "outliers.R","resample.R","base-SBP.R")
+
+SBP.code.files=paste0(SBP.code.path,SBP.code.files)
+
+for (i in 1:length(SBP.code.files))
+  source(SBP.code.files[i])
+
+try(get.package("DescTools"))
+try(get.package("survival"))
+try(get.package("cmprsk"))
+try(get.package("penalized"))
+try(get.package("knitr"))
+try(get.package("data.table"))
+try(get.package("readxl"))
+try(get.package("utils"))
+try(get.package("coin"))
+try(get.package("MatrixModels"))
+try(get.package("riskRegression"))
+#get.package("rsq")
